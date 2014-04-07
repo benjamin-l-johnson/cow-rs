c2dae8f2757538a5ad6c9143abb16168c1c6c90c
Colin Sherratt <colin.sherratt@gmail.com>
2014-04-07 14:36:38 -0400

    fixed #\![]

--- src/cow/lib.rs
+++ src/cow/lib.rs
@@ -1,9 +1,9 @@
-#[crate_id = "github.com/csherratt/cow-rs#cow:0.1"];
-#[comment = "An OpenGL function loader."];
-#[license = "ASL2"];
-#[crate_type = "lib"];
-#[crate_type = "dylib"];
-#[allow(experimental)];
+#![crate_id = "github.com/csherratt/cow-rs#cow:0.1"]
+#![comment = "An OpenGL function loader."]
+#![license = "ASL2"]
+#![crate_type = "lib"]
+#![crate_type = "dylib"]
+#![allow(experimental)]
 
 extern crate sync;
 
